659c6de2d4eb402c1d2d2fd58fa087683a7d7aed
rs rahaman <jesminakter.rahaman@gmail.com>
2023-05-03 19:23:07 +0600

    created the blog pages and added all faq

--- src/routers/PrivateRoutes.jsx
+++ src/routers/PrivateRoutes.jsx
@@ -1,12 +1,10 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
 
-const PrivateRoutes = () => {
-  return (
-    <div>
-      <h2>Private Routes</h2>
-    </div>
-  );
+const PrivateRoutes = ({ children }) => {
+  return <Navigate to="login"></Navigate>;
 };
 
 export default PrivateRoutes;
